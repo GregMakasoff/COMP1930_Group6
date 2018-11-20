bab562afb6598d3adb91e9aef7ebdbb20d26d264
Greg Makasoff <gregmakasoff@gmail.com>
2018-11-20 13:30:28 -0800

    Added Sprint 2 work

---
+++ Sprints/firebaseConnect.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDAQEhk-aNZX6eZTzDPCdwxzvIsgK85rWI",
+    authDomain: "carcar-15571.firebaseapp.com",
+    databaseURL: "https://carcar-15571.firebaseio.com",
+    projectId: "carcar-15571",
+    storageBucket: "",
+    messagingSenderId: "934815323840"
+  };
+  firebase.initializeApp(config); 
